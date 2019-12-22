c60ee92991ab8f489651582fcf073cbfb2cf0c79
Tiago de Bem Natel de Moura <t.nateldemoura@f5.com>
2019-12-22 23:34:58 +0000

    allow http/s
    Signed-off-by: Tiago de Bem Natel de Moura <t.nateldemoura@f5.com>

--- etc/iptables.sh
+++ etc/iptables.sh
@@ -22,6 +22,19 @@ iptables    -A INPUT -p tcp -s 0/0 -d $SERVER_IP --sport 513:65535 \
 iptables    -A OUTPUT -p tcp -s $SERVER_IP -d 0/0 --sport $SSH_PORT \
             --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
 
+# Allow port 80 and 443
+iptables    -A INPUT -p tcp -s 0/0 -d $SERVER_IP --sport 513:65535 \
+            --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+
+iptables    -A OUTPUT -p tcp -s $SERVER_IP -d 0/0 --sport 80 \
+            --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
+
+iptables    -A INPUT -p tcp -s 0/0 -d $SERVER_IP --sport 513:65535 \
+            --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+
+iptables    -A OUTPUT -p tcp -s $SERVER_IP -d 0/0 --sport 443 \
+            --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
+
 # make sure nothing comes or goes out of this box
 iptables -A INPUT -j DROP
 iptables -A OUTPUT -j DROP
