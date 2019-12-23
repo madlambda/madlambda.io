8c67d68905fcb301c6243f9e3260ddedec214947
Tiago de Bem Natel de Moura <t.nateldemoura@f5.com>
2019-12-23 00:32:23 +0000

    add allowed servers
    Signed-off-by: Tiago de Bem Natel de Moura <t.nateldemoura@f5.com>

--- etc/iptables.sh
+++ etc/iptables.sh
@@ -4,16 +4,65 @@ SSH_PORT=1
 IF=eth0
 SERVER_IP=$(ip addr show $IF | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
+DNS_SERVER="100.100.100.16 100.100.100.17"
+
+# Whitelist of domains
+PACKAGE_SERVER="us.archive.ubuntu.com archive.canonical.com security.ubuntu.com www.github.com github.com 140.82.113.3"
+
 iptables -F
 iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
 
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
+for ip in $DNS_SERVER
+do
+	echo "Allowing DNS lookups (tcp, udp port 53) to server '$ip'"
+	iptables -A OUTPUT -p udp -d $ip --dport 53 -m state \
+             --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT  -p udp -s $ip --sport 53 -m state \
+             --state ESTABLISHED     -j ACCEPT
+	iptables -A OUTPUT -p tcp -d $ip --dport 53 -m state \
+             --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT  -p tcp -s $ip --sport 53 -m state \
+             --state ESTABLISHED     -j ACCEPT
+done
+
 # Allow unlimited traffic on loopback
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
+
+for ip in $PACKAGE_SERVER
+do
+	echo "Allow connection to '$ip' on port 21"
+	iptables -A OUTPUT -p tcp -d "$ip" --dport 21  -m state \
+             --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT  -p tcp -s "$ip" --sport 21  -m state \
+             --state ESTABLISHED     -j ACCEPT
+
+    echo "Allow connection to '$ip' on port 22"
+	iptables -A OUTPUT -p tcp -d "$ip" --dport 22  -m state \
+             --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT  -p tcp -s "$ip" --sport 22  -m state \
+             --state ESTABLISHED     -j ACCEPT
+
+	echo "Allow connection to '$ip' on port 80"
+	iptables -A OUTPUT -p tcp -d "$ip" --dport 80  -m state \
+             --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT  -p tcp -s "$ip" --sport 80  -m state \
+             --state ESTABLISHED     -j ACCEPT
+
+	echo "Allow connection to '$ip' on port 443"
+	iptables -A OUTPUT -p tcp -d "$ip" --dport 443 -m state \
+             --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT  -p tcp -s "$ip" --sport 443 -m state \
+             --state ESTABLISHED     -j ACCEPT
+done
 
 # Allow incoming ssh only
 iptables    -A INPUT -p tcp -s 0/0 -d $SERVER_IP --sport 513:65535 \
